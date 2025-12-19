7b91711af599b382e874cccfcc69d4791ab20a70
BrickBldr101 <nrmccracken11@gmail.com>
2025-12-18 22:48:55 -0500

    better install process

--- install.py
+++ install.py
@@ -1,7 +1,21 @@
 # install script
 
 import os
+import sys
 
+print("In order to install explain you need to: install the command to bin and install cowsay")
+yn = input("Continue? [y/n]")
+
+if yn == "y":
+    pass
+elif yn == "n":
+    print("Abort")
+    sys.exit(1)
+else:
+    print("Invalid. Abort")
+    sys.exit(1)
+
+os.system("sudo apt-get install cowsay")
 os.system("chmod +x explain.py")
 os.system("sudo mv explain.py /usr/local/bin/explain")
 
